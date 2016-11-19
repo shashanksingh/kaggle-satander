3f76fdfb614f9bb1cc2b888e645dcafe82d75407
shashank.singh <shashank@faasos.com>
2016-11-20 03:31:48 +0530

    now we have a path

--- parse.py
+++ parse.py
@@ -1,11 +1,20 @@
+#simple collbrative filtering, 
+# step 1 : Find out unique customers
+# step 2 : How many times have they bough a certain product
+# step 3 : find out similar customers ( clustering -> age and other features )
+# stesp 4 : find top hits in a cluster and recommend everyone the same
+
+
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn import cross_validation
 from sklearn.cross_validation import train_test_split
 
-train_v2 = pd.read_csv("train_ver2.csv",header=0, nrows=261)
+print "[Parse] Load CSV"
+train_v2 = pd.read_csv("train_ver2.csv",header=0, nrows=262)
 # print train_v2.pivot(index="ncodpers", columns="fecha_dato", values="age")
 simple_df_to_cluster = train_v2.loc[:,["age"]]
+print "[Parse] GET "
 
 print simple_df_to_cluster
 
--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
-pip install --upgrade pip
+# pip install --upgrade pip
+python -m pip install --upgrade pip
 virtualenv  .
 source bin/activate
-pip install pandas
+pip install  numpy scipy matplotlib ipython jupyter pandas sympy nose
 pip install -U scikit-learn
+pip install pandas
 sudo apt-get install unzip
 wget https://www.kaggle.com/c/santander-product-recommendation/download/train_ver2.csv.zip
 unzip train_ver2.csv.zip
