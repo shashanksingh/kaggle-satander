08d53460be602b12090e610afe739cae71991986
shashank.singh <shashank@faasos.com>
2016-11-22 04:02:37 +0530

    created decisoon tree, now would classify with it tommorow :)

--- clustering.py
+++ clustering.py
@@ -23,15 +23,15 @@ from sklearn.cluster import KMeans
 from sklearn import cross_validation
 from sklearn.cross_validation import train_test_split
 
-print "[Parse][Step 1] Find out unique customers"
-print "[Parse] Load CSV"
+print "[Clustering][Step 1] Find out unique customers"
+print "[Clustering] Load CSV"
 train_v2 = pd.read_csv("train_ver2.csv",header=0, nrows=1262000,dtype={"ncodpers": int})#limited
 customer_codes = train_v2.loc[:,["ncodpers"]]
 unique_customer_codes = customer_codes.drop_duplicates()
-# print "[Parse] Customer Code ", unique_customer_codes
-print "[Parse] Unique customer count", unique_customer_codes.count()
+# print "[Clustering] Customer Code ", unique_customer_codes
+print "[Clustering] Unique customer count", unique_customer_codes.count()
 
-print "[Parse][Step 2] How many times have they bough a certain product"
+print "[Clustering][Step 2] How many times have they bough a certain product"
 customer_with_product_array=train_v2.loc[:,["ncodpers",
           "ind_ahor_fin_ult1","ind_aval_fin_ult1","ind_cco_fin_ult1",
           "ind_cder_fin_ult1","ind_cno_fin_ult1","ind_ctju_fin_ult1",
@@ -42,13 +42,13 @@ customer_with_product_array=train_v2.loc[:,["ncodpers",
           "ind_tjcr_fin_ult1","ind_valo_fin_ult1","ind_viv_fin_ult1",
           "ind_nomina_ult1","ind_nom_pens_ult1","ind_recibo_ult1"
           ]]
-print "[Parse] Groupby customer code"
+print "[Clustering] Groupby customer code"
 customer_with_product_array_grouped_by_customer = customer_with_product_array.groupby(["ncodpers"])
-print "[Parse] Stats - Groupby customer code"
+print "[Clustering] Stats - Groupby customer code"
 product_array_sum = customer_with_product_array_grouped_by_customer.aggregate(np.sum)
 # customer_with_product_array.duplicated()
 
-print "[Parse][Step 3] Find out similar customers ( clustering -> age and other features )"
+print "[Clustering][Step 3] Find out similar customers ( clustering -> age and other features )"
 # cluster = KMeans(n_clusters=10, random_state=20)
 # # result = cluster.fit_predict(unique_customer_code)
 # print "Cluster Centers"
--- decision_tree.py
+++ decision_tree.py
@@ -1,3 +1,15 @@
-from sklearn import tree
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
-clf = tree.DecisionTreeClassifier()
+decision_tree = DecisionTreeClassifier()
+
+print "[DecisonTree][Step 1] Find out unique customers"
+print "[DecisonTree] Load CSV"
+train_v2 = pd.read_csv("train_ver2.csv",header=0, nrows=1262000,dtype={"ncodpers": int})#limited
+customer_codes = train_v2.loc[:,["ncodpers"]]
+product_bought = train_v2.loc[:,["ind_ahor_fin_ult1"]]
+
+clf = decision_tree.fit(customer_codes,product_bought)
+
+export_graphviz(clf, out_file="tree.dot")
