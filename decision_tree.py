7743a2779ae9f5c7c48fa58a354d7fbec22d3193
shashank.singh <shashank@faasos.com>
2016-12-05 19:49:31 +0530

    2 more simple reccomendations

--- decision_tree.py
+++ decision_tree.py
@@ -6,7 +6,7 @@ decision_tree = DecisionTreeClassifier()
 
 print "[DecisonTree][Step 1] Find out unique customers"
 print "[DecisonTree] Load CSV"
-train_v2 = pd.read_csv("train_ver2.csv",header=0, nrows=1262000,dtype={"ncodpers": int})#limited
+train_v2 = pd.read_csv("train_ver2.csv",header=0, nrows=9262000,dtype={"ncodpers": int})#limited
 customer_codes = train_v2.loc[:,["ncodpers"]]
 product_bought = train_v2.loc[:,["ind_ahor_fin_ult1"]]
 
