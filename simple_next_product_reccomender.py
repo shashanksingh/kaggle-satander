b15b6f4ddc76a821264b69985276350e690222f5
shashank.singh <shashank@faasos.com>
2016-12-07 02:52:06 +0530

    randomly generate some opinion of product to be recommended

--- simple_next_product_reccomender.py
+++ simple_next_product_reccomender.py
@@ -4,9 +4,9 @@ import datetime
 
 # train_v2 = pd.read_csv("train_ver2.csv",header=0, nrows=1262000,dtype={"ncodpers": int})#limited
 
-counter = 0 
+counter = 0
 with open('reccomendation.csv', 'a') as f:
-  for df in pd.read_csv('train_ver2.csv',header=0,  chunksize=100000):
+  for df in pd.read_csv('train_ver2.csv',header=0,  chunksize=100):
     customer_with_product_array=df.loc[:,["ncodpers",
           "ind_ahor_fin_ult1","ind_aval_fin_ult1","ind_cco_fin_ult1",
           "ind_cder_fin_ult1","ind_cno_fin_ult1","ind_ctju_fin_ult1",
@@ -20,7 +20,14 @@ with open('reccomendation.csv', 'a') as f:
 
     # customer_with_product_array.
     # for single.
-    customer_with_product_array.to_csv(f,header=False)
+
+    dataframe_to_write = pd.DataFrame([])
+    for row in customer_with_product_array.iterrows():
+      series_to_write = pd.Series({"ncodpers" : row[0], "added_products" : "ind_nomina_ult1"})
+      dataframe_to_write.append(series_to_write,ignore_index=True)
+    print dataframe_to_write
+
+    dataframe_to_write.to_csv(f,header=False)
     counter = counter + 1
     # df.to_csv(f, header=False)
     print "[SIMPLE_NEXT_PRODUCT_RECCOMENDER][", (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")),"][",counter,"] Done with Batch" 
