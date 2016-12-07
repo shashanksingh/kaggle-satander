3c18c43df2afe82c04e91f3c2cdcdc713eef9c6d
shashank.singh <shashank@faasos.com>
2016-12-07 20:38:58 +0530

    fixed dataframe and uniq

--- simple_next_product_reccomender.py
+++ simple_next_product_reccomender.py
@@ -7,7 +7,9 @@ import datetime
 counter = 0
 with open('reccomendation.csv', 'a') as f:
   f.write('ncodpers,added_products\n')
-  for df in pd.read_csv('train_ver2.csv',header=0,  chunksize=10000):
+  ncodpers_dataframe = pd.DataFrame([])
+  duplicate = 0 
+  for df in pd.read_csv('train_ver2.csv',header=0,  chunksize=10):
     customer_with_product_array=df.loc[:,["ncodpers",
           "ind_ahor_fin_ult1","ind_aval_fin_ult1","ind_cco_fin_ult1",
           "ind_cder_fin_ult1","ind_cno_fin_ult1","ind_ctju_fin_ult1",
@@ -19,22 +21,20 @@ with open('reccomendation.csv', 'a') as f:
           "ind_nomina_ult1","ind_nom_pens_ult1","ind_recibo_ult1"
           ]]
 
-    # customer_with_product_array.
-    # for single.
 
-    dataframe_to_write = pd.DataFrame([])
     for row , column in customer_with_product_array.iterrows():
-      # print column[0]
       series_to_write = pd.Series({ "ncodpers":column[0], "added_products":"ind_nomina_ult1"})
-      # print series_to_write, dataframe_to_write
-      # dataframe_to_write.add(series_to_write)
-      # series_to_write.to_csv(f,header=False,sep=',',index=False)
-      f.write(str(int(column[0]))+",ind_nomina_ult1\n")
-    # print dataframe_to_write
+      ncodpers_dataframe = ncodpers_dataframe.append(pd.Series({"ncodpers":column[0]}),ignore_index=True)
+      # print ncodpers_dataframe['ncodpers']
+      #if there is not duplicate then copy
+      if ((ncodpers_dataframe['ncodpers']==column[0]).any())==False:
+        f.write(str(int(column[0]))+",ind_nomina_ult1\n")
+      else:
+        duplicate = duplicate + 1
 
-    print "[SIMPLE_NEXT_PRODUCT_RECCOMENDER][CSV]",dataframe_to_write.to_csv(f,header=False)
+    # print "[SIMPLE_NEXT_PRODUCT_RECCOMENDER][CSV][DuplicateFound]",
     counter = counter + 1
     # df.to_csv(f, header=False)
-    print "[SIMPLE_NEXT_PRODUCT_RECCOMENDER][", (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")),"][",counter,"] Done with Batch" 
+    print "[SIMPLE_NEXT_PRODUCT_RECCOMENDER][", (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")),"][",counter,"] Done with Batch, [Duplicate Found]", duplicate
 
 f.close()
